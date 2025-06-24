4c4028f682889ae2bf08c5d3e55d9f0a85011a34
Xir <prssergey@gmail.com>
2025-06-25 01:58:46 +0300

    refactor: header, dynamic routes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, Route } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
 export class Header {
-  
+  protected routes: Route[] = [];
+
+  constructor(private router: Router) {
+    this.routes = this.router.config.filter(
+      (route) => route.data && route.data['label']
+    );
+  }
 }
