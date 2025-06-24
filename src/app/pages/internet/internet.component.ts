23233439a2c084394baa8e7bbd9c5a43a35f8acf
Xir <prssergey@gmail.com>
2025-06-25 01:30:01 +0300

    feat: internet page

---
+++ src/app/pages/internet/internet.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internet',
+  templateUrl: 'internet.component.html',
+})
+export class InternetPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
