24c04e8c3729bc150d4f5295ed9dc81b169fd4f9
Xir <prssergey@gmail.com>
2025-06-25 01:30:15 +0300

    feat: advanced page

---
+++ src/app/pages/advanced/advanced.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced',
+  templateUrl: 'advanced.component.html',
+})
+export class Advanced implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
