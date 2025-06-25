781acf6e54da6d51eb1d3e271d2883d87c8c13e3
Xir <prssergey@gmail.com>
2025-06-25 12:24:03 +0300

    feat: table event only

---
+++ src/app/features/route-table-old/route-table-old.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../../constant';
+import { compareRoutes } from '../../../utils';
+import { SORTABLE_COLUMNS, SortBy } from '../../types';
+import { TitleCasePipe } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'route-table-old',
+  imports: [TitleCasePipe],
+  templateUrl: './route-table-old.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RouteTableOld {
+  protected sortBy: SortBy = null;
+  protected sortAsc: boolean = true;
+
+  get sortedRoutes() {
+    if (!this.sortBy) return [...routes];
+
+    return [...routes].sort((a, b) =>
+      this.sortAsc
+        ? compareRoutes(a, b, this.sortBy!)
+        : compareRoutes(b, a, this.sortBy!)
+    );
+  }
+
+  readonly SORTABLE_COLUMNS = SORTABLE_COLUMNS;
+
+  toggleSort(column: NonNullable<SortBy>) {
+    if (this.sortBy === column) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortBy = column;
+      this.sortAsc = true;
+    }
+  }
+}
--- src/app/pages/home/home.components.ts
+++ src/app/pages/home/home.components.ts
@@ -3,10 +3,11 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { RouteTableRx } from '../../features/route-table-rx/route-table.component';
 import { RouteTable } from '../../features/route-table/route-table.component';
+import { RouteTableOld } from '../../features/route-table-old/route-table-old.component';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [RouteTableRx, RouteTable, MatTabsModule],
+  imports: [RouteTableRx, RouteTable, RouteTableOld, MatTabsModule],
 })
 export class HomePage {}
