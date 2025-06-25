0e3a72e09b5c93da3dcafc8faeb4f9383ac464d1
Xir <prssergey@gmail.com>
2025-06-25 12:23:38 +0300

    styles: btn, home content

--- src/app/features/theme-switch/theme-switch.component.ts
+++ src/app/features/theme-switch/theme-switch.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-theme-switch',
   templateUrl: 'theme-switch.component.html',
+  imports: [MatButtonModule],
 })
 export class ThemeSwitch implements OnInit {
   theme: 'light' | 'dark' = window.matchMedia('(prefers-color-scheme: dark)')
